eb646ef80c1b14ff0a2cb977531770c98bcbb8c9
mickaelulivieri <mmickael.carvalho@gmail.com>
2025-02-17 10:14:31 -0300

    feat: desenvolvendo primeiramente a classe category

---
+++ src/main/java/com/movieflix/movieflix/controller/CategoryController.java
@@ -0,0 +1,25 @@
+package com.movieflix.movieflix.controller;
+
+import com.movieflix.movieflix.entity.Category;
+import com.movieflix.movieflix.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("/movieflix/category")
+public class CategoryController {
+
+    //injecao de dependencia
+    @Autowired
+    private CategoryService categoryService;
+
+    //metodos
+
+    public List<Category> getAllCategories(){
+        return categoryService.findAll
+    }
+}
---
+++ src/main/java/com/movieflix/movieflix/service/CategoryService.java
@@ -0,0 +1,17 @@
+package com.movieflix.movieflix.service;
+
+import com.movieflix.movieflix.entity.Category;
+import com.movieflix.movieflix.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+    //injecao de dependencia
+    private CategoryRepository categoryRepository;
+
+    //metodos
+    public List<Category> findAll(){
+        return
+    }
+}
