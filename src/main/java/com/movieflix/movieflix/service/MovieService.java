c7eea1643825742f966f4a3f09fa10cd90a592ce
mickaelulivieri <mmickael.carvalho@gmail.com>
2025-02-24 16:51:00 -0300

    desenvolvimento tratamento de straming

--- src/main/java/com/movieflix/movieflix/service/MovieService.java
+++ src/main/java/com/movieflix/movieflix/service/MovieService.java
@@ -1,10 +1,12 @@
 package com.movieflix.movieflix.service;
 
+import com.movieflix.movieflix.entity.Category;
 import com.movieflix.movieflix.entity.Movie;
 import com.movieflix.movieflix.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,14 +14,23 @@ import java.util.List;
 public class MovieService {
 
     private final MovieRepository movieRepository;
+    private final CategoryService categoryService;
 
     public Movie save(Movie movie){
-        return movieRepository.save(movie);
+        this.findCategories(movie.getCategories());
+    return movieRepository.save(movie);
     }
 
     public List<Movie> findAll(){
         return movieRepository.findAll();
     }
 
+    private List<Category> findCategories(List<Category> categories){
+        List<Category> categoriesFound = new ArrayList<>();
+        for (Category category : categories){
+            categoryService.findById(category.getId()).ifPresent(categoriesFound::add);
+        }
+         return categoriesFound;
+    }
 
 }
