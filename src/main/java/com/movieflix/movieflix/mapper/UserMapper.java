772ef99a4a973aa154f634357af7e035630cdf60
mickaelulivieri <mmickael.carvalho@gmail.com>
2025-02-26 15:35:54 -0300

    refactor: corrigindo a ausencia de getId no mapper do user

--- src/main/java/com/movieflix/movieflix/mapper/UserMapper.java
+++ src/main/java/com/movieflix/movieflix/mapper/UserMapper.java
@@ -19,6 +19,7 @@ public class UserMapper {
 
     public static UserResponse toUserResponse(User user){
         return UserResponse.builder()
+                .id(user.getId())
                 .name(user.getName())
                 .email(user.getEmail())
                 .build();
