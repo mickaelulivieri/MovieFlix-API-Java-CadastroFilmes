18a38dddf4f3fa106124d29740fd44ebca5c5c6e
mickaelulivieri <mmickael.carvalho@gmail.com>
2025-02-26 16:06:19 -0300

    feat: primeira parte da security configurada, com user e config

---
+++ src/main/java/com/movieflix/movieflix/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.movieflix.movieflix.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize.
+                        requestMatchers(HttpMethod.POST, "/movieflix/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/movieflix/auth/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+
+}
